78e1d9887d49581ea140ea3938ac537b3fd72cc1
KBergman <KBergman@VCG-KEVINB>
2016-08-08 07:21:05 -0500

    bypass load balancer (hopefullly for now)

--- 0-Common/SampleApp/VDC-dbConfig.js
+++ 0-Common/SampleApp/VDC-dbConfig.js
@@ -1,2 +1,2 @@
-﻿var dbConfig = { config : { "user": "MSFTAzureARM", "password": "rQ53uUn3rm", "server": "10.0.8.250", "database": "FabrikamFiber" } };
+﻿var dbConfig = { config : { "user": "MSFTAzureARM", "password": "rQ53uUn3rm", "server": "10.0.8.5", "database": "FabrikamFiber" } };
 module.exports = dbConfig;
